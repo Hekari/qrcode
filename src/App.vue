fc69004da25cf75be9123c850013634b4745b9b4
Hekari <sszpytma6@gmail.com>
2022-08-31 16:48:24 +0200

    asd

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,54 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="QRBlock">
+    <img class="qrcodeimg" src="./assets/image-qr-code.png" alt="qrcode">
+    <h1>Lorem, ipsum.</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque, et.
+    </p>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: 'App'
 }
 </script>
 
 <style>
+  body{
+    background-color: hsl(212, 45%, 89%);
+  }
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  font-family: 'Outfit', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+.QRBlock{
+  font-size: 15px;
+  border-radius: 15px;
+  height: 65vh;
+  width: 75vw;
+  background-color: hsl(0, 0%, 100%);
+}
+.qrcodeimg{
+  margin-top: 2vh;
+  width: 65vw;
+  border-radius: 12px;
+}
+
+@media screen and (min-width: 1024px) {
+  .QRBlock{
+    height: 65vh;
+    width: 20vw;
+  }
+  .qrcodeimg{
+    width: 19vw;
+  }
 }
 </style>
